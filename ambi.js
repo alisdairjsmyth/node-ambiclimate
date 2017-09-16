5c23f823cd05118c7b063136f3d1c544db965acf
alisdairjsmyth <alisdair.smyth@gmail.com>
2017-09-16 23:08:49 +1000

    Updated formatting

--- ambi.js
+++ ambi.js
@@ -27,186 +27,186 @@ var Q = require('q');
 var _ = require('lodash');
 
 function Client(clientId, clientSecret, username, password) {
-    var ambiAuth = require('./ambi-auth');
+  var ambiAuth = require('./ambi-auth');
 
-    this.settings = {
-        baseUrl: 'https://api.ambiclimate.com/api/v1',
-        bearerToken: null
-    };
+  this.settings = {
+    baseUrl: 'https://api.ambiclimate.com/api/v1',
+    bearerToken: null
+  };
 
-    this.auth = new ambiAuth(clientId, clientSecret, username, password);
+  this.auth = new ambiAuth(clientId, clientSecret, username, password);
 
-    return this;
+  return this;
 }
 
 Client.prototype.send = function(settings, cb) {
-    this.auth.getToken( (token) => {
-        var defaults = {
-            qs: {},
-            json: true,
-            method: 'GET',
-            baseUrl: this.settings.baseUrl,
-            auth: {
-                'bearer': token
-            }
+  this.auth.getToken( (token) => {
+    var defaults = {
+        qs: {},
+        json: true,
+        method: 'GET',
+        baseUrl: this.settings.baseUrl,
+        auth: {
+            'bearer': token
         }
+    }
 
-        settings = settings || {};
-        settings = _.merge(defaults, settings);
+    settings = settings || {};
+    settings = _.merge(defaults, settings);
 
-        request(settings, function(err, response, body) {
-            if (err) {
-                cb(err, null);
-                return;
-            }
-            // Compensation for issue with underlying API always returning success
-            if (body.reason) {
-              cb(body, null)
-              return;
-            }
-            cb(null, body);
-        });
+    request(settings, function(err, response, body) {
+      if (err) {
+        cb(err, null);
+        return;
+      }
+      // Compensation for issue with underlying API always returning success
+      if (body.reason) {
+        cb(body, null)
+        return;
+      }
+      cb(null, body);
     });
+  });
 }
 
 Client.prototype.off = function(settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/power/off',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/power/off',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.comfort = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/mode/comfort',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/mode/comfort',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.feedback = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/user/feedback',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/user/feedback',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.away_temperature_lower = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/mode/away_temperature_lower',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/mode/away_temperature_lower',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.away_temperature_upper = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/mode/away_temperature_upper',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/mode/away_temperature_upper',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.away_humidity_upper = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/mode/away_humidity_upper',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/mode/away_humidity_upper',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.temperature = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/mode/temperature',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/mode/temperature',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.sensor_temperature = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/sensor/temperature',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/sensor/temperature',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.sensor_humidity = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/sensor/humidity',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/sensor/humidity',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 Client.prototype.mode = function (settings, cb) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    this.send({
-        url: '/device/mode',
-        qs: settings
-    }, function(err, data) {
-        if (err) deferred.reject(err);
-        else deferred.resolve(data);
-    });
+  this.send({
+    url: '/device/mode',
+    qs: settings
+  }, function(err, data) {
+    if (err) deferred.reject(err);
+    else deferred.resolve(data);
+  });
 
-    return deferred.promise.nodeify(cb);
+  return deferred.promise.nodeify(cb);
 }
 
 module.exports = Client;
