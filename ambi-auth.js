44946147e8f8ad1522f1c5ea1e5412f6ab45a831
alisdairjsmyth <alisdair.smyth@gmail.com>
2017-12-12 16:57:38 +1100

    Prettier formatting

--- ambi-auth.js
+++ ambi-auth.js
@@ -1,170 +1,159 @@
-'use strict';
-var request = require('request');
-var simpleoauth2 = require('simple-oauth2');
-var url = require('url');
-
+"use strict";
+var request = require("request");
+var simpleoauth2 = require("simple-oauth2");
+var url = require("url");
 
 class AmbiAuthorizationCode {
+  // Requires from https://api.ambiclimate.com/
+  //
+  // Client ID
+  // Client Secret
+  //
+  // AmbiClimate User e-mail
+  // AmbiClimate User Password
+  //
+  // var api = new AmbiAuthorizationCode('id', 'secret', 'user@mail.com', 'pass');
+  //
+  // api.getToken( (token) => {
+  //         console.log(token);
+  //      });
+  //
 
-    // Requires from https://api.ambiclimate.com/
-    //
-    // Client ID
-    // Client Secret
-    //
-    // AmbiClimate User e-mail
-    // AmbiClimate User Password
-    //
-    // var api = new AmbiAuthorizationCode('id', 'secret', 'user@mail.com', 'pass');
-    //
-    // api.getToken( (token) => {
-    //         console.log(token);
-    //      });
-    //
-
-    constructor(clientid, clientsecret, username, password) {
-        const credentials = {
-            client: {
-            id: clientid,
-            secret: clientsecret,
-        },
-        auth: {
-            tokenHost: 'https://api.ambiclimate.com',
-            tokenPath: '/oauth2/token',
-            authorizePath: '/oauth2/authorize',
-        },
-        http: {
-            headers: { 'Accept': 'application/json' }
-        }};
-
-        this._oauth2 = simpleoauth2.create(credentials);
-
-        this.auth = this.authenticate(username, password);
-    }
-
-    // Obtains the token once, caches, and refreshes if it has expired
-    // callback(token):
-
-    getToken(callback) {
-        this.promiseToGetToken()
-        .then((token) => {
-              callback(token.token.access_token)
-        })
-    }
-
-
-    promiseToGetToken() {
-        return this.auth.then((token) => {
-            if (token.expired()) {
-                return token.refresh();
-            }
-                return token;
-        })
-
-    }
-
-    // Authentication
-    //
-
-    authenticate(username, password) {
-        this.auth = this.authenticate_auth_code_flow(username, password);
-        return this.auth;
+  constructor(clientid, clientsecret, username, password) {
+    const credentials = {
+      client: {
+        id: clientid,
+        secret: clientsecret
+      },
+      auth: {
+        tokenHost: "https://api.ambiclimate.com",
+        tokenPath: "/oauth2/token",
+        authorizePath: "/oauth2/authorize"
+      },
+      http: {
+        headers: { Accept: "application/json" }
+      }
     };
 
-    // Authentiate using OAuth2 authorization code flow
+    this._oauth2 = simpleoauth2.create(credentials);
 
-    authenticate_auth_code_flow(username, password) {
+    this.auth = this.authenticate(username, password);
+  }
 
-        const authorizationUri = this._oauth2.authorizationCode.authorizeURL({
-             scope: 'email device_read ac_control',
-             });
+  // Obtains the token once, caches, and refreshes if it has expired
+  // callback(token):
 
-        const promiseToRequest = AmbiAuthorizationCode.promiseToRequest;
-        const oauth2 = this._oauth2;
+  getToken(callback) {
+    this.promiseToGetToken().then(token => {
+      callback(token.token.access_token);
+    });
+  }
 
-        // Need to retain a cookie during login process
+  promiseToGetToken() {
+    return this.auth.then(token => {
+      if (token.expired()) {
+        return token.refresh();
+      }
+      return token;
+    });
+  }
 
-        var j = request.jar();
+  // Authentication
+  //
 
-        // First initiate an authorization request for this APP
+  authenticate(username, password) {
+    this.auth = this.authenticate_auth_code_flow(username, password);
+    return this.auth;
+  }
 
-        return promiseToRequest(authorizationUri)
-        .then(function(response) {
+  // Authentiate using OAuth2 authorization code flow
 
-              var loginRequest = {
-                  url: 'https://api.ambiclimate.com/login',
-                  method: 'POST',
-                  jar: j,
-                  form: {email: username, password: password}
-              }
+  authenticate_auth_code_flow(username, password) {
+    const authorizationUri = this._oauth2.authorizationCode.authorizeURL({
+      scope: "email device_read ac_control"
+    });
 
-              // Then login to the ambiclimate API with the username and password
+    const promiseToRequest = AmbiAuthorizationCode.promiseToRequest;
+    const oauth2 = this._oauth2;
 
-              return promiseToRequest(loginRequest);
-            })
+    // Need to retain a cookie during login process
 
-        .then(function(response) {
+    var j = request.jar();
 
+    // First initiate an authorization request for this APP
 
-              var authRequest = {
-                  uri: authorizationUri,
-                  method: 'POST',
-                  form: {confirm: 'yes'},
-                  jar: j,
-              }
+    return promiseToRequest(authorizationUri)
+      .then(function(response) {
+        var loginRequest = {
+          url: "https://api.ambiclimate.com/login",
+          method: "POST",
+          jar: j,
+          form: { email: username, password: password }
+        };
 
-              // Then confirm the user allows this app to access their ambiclimate units
-              // which returns an authorization code
+        // Then login to the ambiclimate API with the username and password
 
-              return promiseToRequest(authRequest);
-            })
+        return promiseToRequest(loginRequest);
+      })
 
+      .then(function(response) {
+        var authRequest = {
+          uri: authorizationUri,
+          method: "POST",
+          form: { confirm: "yes" },
+          jar: j
+        };
 
-        .then(function(response) {
+        // Then confirm the user allows this app to access their ambiclimate units
+        // which returns an authorization code
 
-              var code = url.parse(response.headers.location, true).query.code;
+        return promiseToRequest(authRequest);
+      })
 
-              // Get the access token object (the authorization code is given from the previous step).
-              const tokenConfig = {
-                  code: code,
-                  redirect_uri: 'https://httpbin.org/get'
-              };
+      .then(function(response) {
+        var code = url.parse(response.headers.location, true).query.code;
 
-              // Save the access token
+        // Get the access token object (the authorization code is given from the previous step).
+        const tokenConfig = {
+          code: code,
+          redirect_uri: "https://httpbin.org/get"
+        };
 
-              return oauth2.authorizationCode.getToken(tokenConfig)
-            })
+        // Save the access token
 
-        .then((result) => {
+        return oauth2.authorizationCode.getToken(tokenConfig);
+      })
 
-              const token = oauth2.accessToken.create(result);
-              return token;
+      .then(result => {
+        const token = oauth2.accessToken.create(result);
+        return token;
+      })
 
-            })
+      .catch(error => {
+        console.log(error);
+        console.log("Access Token Error", error.message);
+        return nil;
+      });
+  }
 
-        .catch((error) => {
-               console.log(error)
-               console.log('Access Token Error', error.message);
-               return nil
-               });
-    };
+  // Create a promise to access a URL with associated options and headers
+  // a status 200 or 302 will resolve the response
+  // any other status will cause a rejection
 
-    // Create a promise to access a URL with associated options and headers
-    // a status 200 or 302 will resolve the response
-    // any other status will cause a rejection
-
-    static promiseToRequest(options) {
-
-        return new Promise(function(resolve, reject) {
-           request(options, function (error, response, body) {
-               if (!error && (response.statusCode == 200 || response.statusCode == 302)) {
-                   resolve(response);
-               } else {
-                   reject(response);
-               }
-           })
-       })
-    };
-
+  static promiseToRequest(options) {
+    return new Promise(function(resolve, reject) {
+      request(options, function(error, response, body) {
+        if (
+          !error &&
+          (response.statusCode == 200 || response.statusCode == 302)
+        ) {
+          resolve(response);
+        } else {
+          reject(response);
+        }
+      });
+    });
+  }
 }
 
 module.exports = AmbiAuthorizationCode;
--- ambi.js
+++ ambi.js
@@ -20,17 +20,17 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-**/
-'use strict';
-var request = require('request');
-var Q = require('q');
-var _ = require('lodash');
+ **/
+"use strict";
+var request = require("request");
+var Q = require("q");
+var _ = require("lodash");
 
 function Client(clientId, clientSecret, username, password) {
-  var ambiAuth = require('./ambi-auth');
+  var ambiAuth = require("./ambi-auth");
 
   this.settings = {
-    baseUrl: 'https://api.ambiclimate.com/api/v1',
+    baseUrl: "https://api.ambiclimate.com/api/v1",
     bearerToken: null
   };
 
@@ -40,16 +40,16 @@ function Client(clientId, clientSecret, username, password) {
 }
 
 Client.prototype.send = function(settings, cb) {
-  this.auth.getToken( (token) => {
+  this.auth.getToken(token => {
     var defaults = {
-        qs: {},
-        json: true,
-        method: 'GET',
-        baseUrl: this.settings.baseUrl,
-        auth: {
-            'bearer': token
-        }
-    }
+      qs: {},
+      json: true,
+      method: "GET",
+      baseUrl: this.settings.baseUrl,
+      auth: {
+        bearer: token
+      }
+    };
 
     settings = settings || {};
     settings = _.merge(defaults, settings);
@@ -61,210 +61,252 @@ Client.prototype.send = function(settings, cb) {
       }
       // Compensation for issue with underlying API always returning success
       if (body.reason) {
-        cb(body, null)
+        cb(body, null);
         return;
       }
       cb(null, body);
     });
   });
-}
+};
 
 Client.prototype.off = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/power/off',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/power/off",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.comfort = function (settings, cb) {
+Client.prototype.comfort = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/mode/comfort',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/mode/comfort",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.feedback = function (settings, cb) {
+Client.prototype.feedback = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/user/feedback',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/user/feedback",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.away_temperature_lower = function (settings, cb) {
+Client.prototype.away_temperature_lower = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/mode/away_temperature_lower',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/mode/away_temperature_lower",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.away_temperature_upper = function (settings, cb) {
+Client.prototype.away_temperature_upper = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/mode/away_temperature_upper',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/mode/away_temperature_upper",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.away_humidity_upper = function (settings, cb) {
+Client.prototype.away_humidity_upper = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/mode/away_humidity_upper',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/mode/away_humidity_upper",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.temperature = function (settings, cb) {
+Client.prototype.temperature = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/mode/temperature',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/mode/temperature",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.sensor_temperature = function (settings, cb) {
+Client.prototype.sensor_temperature = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/sensor/temperature',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/sensor/temperature",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.sensor_humidity = function (settings, cb) {
+Client.prototype.sensor_humidity = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/sensor/humidity',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/sensor/humidity",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.mode = function (settings, cb) {
+Client.prototype.mode = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/mode',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/mode",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.appliance_states = function (settings, cb) {
+Client.prototype.appliance_states = function(settings, cb) {
   var deferred = Q.defer();
 
   // Default limit and offset
-  settings.limit  = (settings.limit)  ? settings.limit  : 5;
-  settings.offset = (settings.offset) ? settings.offset : 0;
+  settings.limit = settings.limit ? settings.limit : 5;
+  settings.offset = settings.offset ? settings.offset : 0;
 
-  this.send({
-    url: '/device/appliance_states',
-    qs: settings
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/appliance_states",
+      qs: settings
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.devices = function (cb) {
+Client.prototype.devices = function(cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/devices'
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/devices"
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.ir_feature = function (settings, cb) {
+Client.prototype.ir_feature = function(settings, cb) {
   var deferred = Q.defer();
 
-  this.send({
-    url: '/device/ir_feature'
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  this.send(
+    {
+      url: "/device/ir_feature"
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
-Client.prototype.deployment = function (settings, cb) {
+Client.prototype.deployment = function(settings, cb) {
   var deferred = Q.defer();
 
-  settings.method = 'POST';
-  this.send({
-    url: '/device/deployment'
-  }, function(err, data) {
-    if (err) deferred.reject(err);
-    else deferred.resolve(data);
-  });
+  settings.method = "POST";
+  this.send(
+    {
+      url: "/device/deployment"
+    },
+    function(err, data) {
+      if (err) deferred.reject(err);
+      else deferred.resolve(data);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
-}
+};
 
 module.exports = Client;
